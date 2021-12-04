998750f68f47244afadbbc443915cbb934bd3598
niranj <niranj2508@gmail.com>
2021-12-04 10:48:03 +0530

    initial commit

---
+++ uhic/uhic/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+def home(request):
+    return render(request, "home.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def signup(request):
+    return render(request,"signup.html")
+
+def input1(request):
+    return render(request,"input1.html")
+
+def input2(request):
+    return render(request,"input2.html")
+
+def input3(request):
+    return render(request,"input3.html")
